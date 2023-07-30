71050de2410b4ed09f6e1ba2d3994787637b21b0
PHUMLA Lumatha <phumalumatha@yahoo.com>
2023-07-30 13:46:19 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import './App.css';
+import React, { useState } from 'react';
+
+export default function App() {
+  const [list, setList] = useState([]);
+  const [input, setInput] = useState('');
+
+  const addTodo = (todo) => {
+    const newTodo = {
+      id: Math.random(),
+      todo: todo
+    };
+
+    // to add todo on the list
+    setList([...list, newTodo]);
+
+    // to clear input box
+    setInput('');
+  };
+
+  const deleteTodo = (id) => {
+    // filter out todo with the id
+    const newList = list.filter((todo) => todo.id !== id);
+
+    setList (newList);
+  };
+
+  return (
+    <div>
+      <h1>Todo List</h1>
+      <input 
+        type="text" 
+        value={input} 
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <button onClick={() => addTodo(input)}>Add</button>
+      <ul>
+        {list.map((todo) => (
+          <li key={todo.id}>
+            {todo.todo}
+            <button>&times;</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
